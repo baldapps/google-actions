2442b945f8aa72f18718b6fba9611253315ad85c
baldapps <playappassistance@gmail.com>
2017-07-17 20:34:10 +0200

    Fix headers

--- src/main/java/com/balda/AppConfiguration.java
+++ src/main/java/com/balda/AppConfiguration.java
@@ -3,18 +3,18 @@
  * 
  * This file is part of Google Actions project
  *
- * AirTask Desktop is free software: you can redistribute it and/or modify
+ * Google Actions is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * AirTask Desktop is distributed in the hope that it will be useful,
+ * Google Actions is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with AirTask Desktop.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Google Actions.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.balda;
